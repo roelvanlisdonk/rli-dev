092e7cf89cd733a53098afffde88eb436a0cbea5
Roel van Lisdonk <roelvanlisdonk@live.com>
2019-12-24 14:02:22 +0100

    x

--- projects/native/typescript/frontend/src/app.ts
+++ projects/native/typescript/frontend/src/app.ts
@@ -1,52 +1,144 @@
 import { setupReloadOnServerSideEvent } from './services/server-side-events.service';
-import { addCssTag } from './services/style.service';
+import { addCssTag, addCssClass } from './services/style.service';
 
 setupReloadOnServerSideEvent();
-renderAppComponent();
+boot();
 
-function renderAppComponent() {
-  const name = 'ts-app';
+function boot() {
+  const persons: Person[] = [
+    {
+      firstName: 'Max',
+      lastName: 'Brook'
+    }
+  ];
+
+  appendComponent(document.body, personList(persons));
+}
+
+function appendComponent(element: HTMLElement, component: Component): void {
+  const childElement = document.createElement(component.name);
+
+  if (component.text && typeof component.text == 'string') {
+    childElement.textContent = component.text;
+  }
+
+  if (component.type && typeof component.type == 'string') {
+    (childElement as HTMLButtonElement).type = component.type;
+  }
+
+  appendComponentClasses(childElement, component);
+  appendComponentChildren(childElement, component);
+  element.appendChild(childElement);
+}
+
+function appendComponentClasses(childElement: HTMLElement, component: Component) {
+  if (component.classes && component.classes.length > 0) {
+    const total = component.classes.length;
+    for (let i = 0; i < total; i++) {
+      const cssClass = component.classes[i];
+      if (typeof cssClass == 'string') {
+        childElement.classList.add(cssClass);
+      }
+    }
+  }
+}
+
+function appendComponentChildren(childElement: HTMLElement, component: Component) {
+  if (component.children && component.children.length > 0) {
+    const total = component.children.length;
+    for (let i = 0; i < total; i++) {
+      const grandChild = component.children[i];
+      if (isComponent(grandChild)) {
+        appendComponent(childElement, grandChild);
+      } else {
+        appendComponentsBinding(childElement, grandChild);
+      }
+    }
+  }
+}
+
+function appendComponentsBinding(childElement: HTMLElement, binding: Binding<any, Component[]>) {
+  const components = binding.fn(binding.deps);
+  const total = components.length;
+  for (let i = 0; i < total; i++) {
+    const component = components[i];
+    appendComponent(childElement, component);
+  }
+}
+
+function isComponent(component: Component | Binding<any, Component[]>): component is Component {
+  return (component as Component).name !== undefined;
+}
+
+export interface ActionButtonOptions {
+  text: string | Binding<any, string>;
+}
+
+function actionButton(options: ActionButtonOptions): Component {
+  const name = 'button';
+  const className = 'action-button';
+  addCssClass(className, {
+    cursor: 'pointer',
+    userSelect: 'none'
+  });
+
+  return {
+    name,
+    classes: [className],
+    text: options.text,
+    type: 'button'
+  };
+}
+
+function personList(persons: Person[]): Component {
+  const name = 'person-list';
   addCssTag(name, {
     border: '10px solid rgb(100, 100, 100)',
     display: 'flex'
   });
-
-  const fragment = document.createDocumentFragment();
-  const hello = document.createElement('ts-app');
-
-  hello.textContent = 'Hello world!!!!';
-  fragment.append(hello);
-  document.body.appendChild(fragment);
-}
-
-function personList(persons: Person[], overrides?: VNode): VNode {
-  const name = 'person-list';
   return {
     name,
     children: [
+      actionButton({ text: 'Execute' }),
       {
         name: 'ul',
-        children: [{}]
+        children: [personListItems(persons)]
       }
     ]
   };
 }
+
+function personListItems(persons: Person[]): Binding<Person[], Component[]> {
+  persons = persons || [];
+  return {
+    deps: persons,
+    fn: (persons: Person[]) =>
+      persons.map((x) => {
+        return {
+          name: 'li',
+          text: x.firstName
+        };
+      })
+  };
+}
+
+export type ComponentFactory<T> = (deps: T, overrides?: Component) => Component;
 
 export interface Person {
   firstName: string;
   lastName: string;
 }
 
-export interface VNode {
-  children?: (VNode | Binding<any, VNode[]>)[];
+export interface Component {
+  children?: (Component | Binding<any, Component[]>)[];
   classes?: (string | Binding<any, any>)[];
   name: string;
-  text?: string | Binding<any, any>;
+  text?: string | Binding<any, string>;
+  type?: string;
 }
 
 export interface Binding<T, V> {
   deps: T;
-  depFields: any; // Only used, to improve performance, when you only want to re-render on changes of specific fields.
-  value: V;
+  depFields?: keyof T[]; // Only used, to improve performance, when you only want to re-render on changes of specific fields.
   fn: (deps: T) => V;
 }
