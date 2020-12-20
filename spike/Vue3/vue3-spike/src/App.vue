3590a2305a4e50743a21e9d3dc3fa2a27dabeae9
Roel van Lisdonk <roelvanlisdonk@live.com>
2020-12-20 15:58:14 +0100

    x

---
+++ spike/Vue3/vue3-spike/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome roel to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+export class MyTestCar {
+
+}
+
+const x = new MyTestCar();
+
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 40px;
+}
+</style>
